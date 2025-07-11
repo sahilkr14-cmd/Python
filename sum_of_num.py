736a70d159602a7b649ecb06fa21cf797b91db3c
sahilkr14-cmd <sahil5678sharma@gmail.com>
2025-07-12 04:15:17 +0530

    Code Updating
    Change some Code Lines and make it cleaner and Shorter

--- sum_of_num.py
+++ sum_of_num.py
@@ -1,8 +1,7 @@
-def sum_array(arr):
 def sum(arr):
     total = 0
     for val in arr:
         total += val
     return total
 
-print(sum_array([1, 2, 3]))  # Output: 6print(sum([5, 4 ,6 ,8]))
+print(sum([5, 4 ,6 ,8]))
