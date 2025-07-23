9a861f459a64ad1a9b6c5c8bcee7484fbd8c12cf
sahilkr14-cmd <sahil5678sharma@gmail.com>
2025-07-23 21:12:56 +0530

    Update Problem#3.py

--- Problem#3.py
+++ Problem#3.py
@@ -7,8 +7,8 @@ def maxSubArray(nums):
     max_sum = nums[0]
     
     for num in nums[1:]:
-        current_sum = max(num, current_sum + num)  # extend or start new
-        max_sum = max(max_sum, current_sum)        # update global max
+        current_sum = max(num, current_sum + num)
+        max_sum = max(max_sum, current_sum)
     
     return max_sum
 
