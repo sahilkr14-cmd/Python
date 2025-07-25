5a6015b66b34802af9d0423e949a4769d521d202
sahilkr14-cmd <sahil5678sharma@gmail.com>
2025-07-25 22:10:54 +0530

    Update Problem#7.py

--- Problem#7.py
+++ Problem#7.py
@@ -60,7 +60,6 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode()
         tail = dummy
