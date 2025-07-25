5c118a63f55f5ce0ddcbb5fd56bc59a478a0a119
sahilkr14-cmd <sahil5678sharma@gmail.com>
2025-07-25 22:32:18 +0530

    Valid Parentheses

---
+++ Problem#8.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
+
+        for char in s:
+            if char in mapping.values():
+                stack.append(char)
+            elif char in mapping:
+                if not stack or stack[-1] != mapping[char]:
+                    return False
+                stack.pop()
+            else:
+                return False
+
+        return len(stack) == 0
+
+
+print(Solution().isValid("()"))
+print(Solution().isValid("()[]{}"))
+print(Solution().isValid("([)]"))
+print(Solution().isValid("{[]}"))
