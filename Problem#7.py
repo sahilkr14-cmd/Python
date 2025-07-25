c410f58ff77b9d33ce78bbba5e889218c0448912
sahilkr14-cmd <sahil5678sharma@gmail.com>
2025-07-25 22:05:19 +0530

    Update Problem#7.py

--- Problem#7.py
+++ Problem#7.py
@@ -51,3 +51,29 @@ l2 = array_to_linked_list([1, 3, 4])
 
 merged = mergeTwoLists(l1, l2)
 print(linked_list_to_array(merged))
+
+
+
+### This is Code is LeetCode Solution code
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        tail = dummy
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        if list1:
+            tail.next = list1
+        if list2:
+            tail.next = list2
+        return dummy.next
