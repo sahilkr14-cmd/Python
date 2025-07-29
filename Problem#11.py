a37cefdab7b1cb8500fec0f7a593c8e882ed8a1b
sahilkr14-cmd <sahil5678sharma@gmail.com>
2025-07-29 22:16:17 +0530

    Remove Duplicates from Shorted Array

---
+++ Problem#11.py
@@ -0,0 +1,18 @@
+class Solution:
+    def removeDuplicates(self, nums):
+        if not nums:
+            return 0
+
+        i = 0  # i points to last unique element
+
+        for j in range(1, len(nums)):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+
+        return i + 1
+
+nums = [0,0,1,1,1,2,2,3,3,4]
+k = Solution().removeDuplicates(nums)
+print("k =", k)
+print("Modified nums =", nums[:k])
