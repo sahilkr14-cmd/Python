3e61182cb3c7983a5387d72b4436a53816854269
sahilkr14-cmd <sahil5678sharma@gmail.com>
2025-07-29 22:17:07 +0530

    Update Problem#1.py

--- Problem#1.py
+++ Problem#1.py
@@ -1,5 +1,6 @@
 # Leetcode #1
-# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# Given an array of integers nums and an integer target, 
+# return indices of the two numbers such that they add up to target.
 
 def twosum(nums, target):
     num_map = {}
